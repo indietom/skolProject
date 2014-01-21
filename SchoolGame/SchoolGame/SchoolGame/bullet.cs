38b2fedd3480d6590ed18e1c5c338f0599f46630
Deacro <robin.ingerheim@hotmail.com>
2014-01-21 09:17:01 +0100

    shoot

---
+++ SchoolGame/SchoolGame/SchoolGame/bullet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SchoolGame
+{
+    class bullet : objects
+    {
+        public bullet(float x2, float y2)
+        {
+            setCoords(x2, y2);
+            setSpriteCoords(34, 1);
+            setSize(3, 3);
+            destroy = false;
+        }
+        public void movment()
+        {
+            x += 10;
+        }
+
+    }
+}
