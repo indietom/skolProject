d2be7eac6bc77cc4858531db5a52fd9907b1a915
indietom <tom.leo@hotmail.com>
2014-01-21 09:08:11 +0100

    codez

---
+++ SchoolGame/SchoolGame/SchoolGame/objects.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace SchoolGame
+{
+    class objects
+    {
+        public bool destroy;
+
+        public float x;
+        public float y;
+        public int imx;
+        public int imy;
+        public int widht;
+        public int height;
+        public int hp;
+
+        public int animationCount;
+
+        public bool animationActive;
+        public float angle2;
+        public float angle;
+        public float speed;
+        public float scale_x;
+        public float scale_y;
+        public float veclocity_x;
+        public float veclocity_y;
+
+        public void setCoords(float x2, float y2)
+        {
+            x = x2;
+            y = y2;
+        }
+        public void setSpriteCoords(int imx2, int imy2)
+        {
+            imx = imx2;
+            imy = imy2;
+        }
+        public void setSize(int w2, int h2)
+        {
+            widht = w2;
+            height = h2;
+        }
+        public void drawSprite(SpriteBatch spriteBatch, Texture2D spritesheet)
+        {
+            spriteBatch.Draw(spritesheet, new Vector2(x, y), new Rectangle(imx, imy, widht, height), Color.White);
+        }
+        public void math(int speed2)
+        {
+            angle2 = (angle * (float)Math.PI / 180);
+            speed = speed2;
+            veclocity_x = (speed * (float)Math.Cos(angle));
+            veclocity_y = (speed * (float)Math.Sin(angle));
+        }
+    }
+}
---
+++ SchoolGame/SchoolGame/SchoolGame/player.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace SchoolGame
+{
+    class player:objects
+    {
+        public int gunType;
+        public int fireRate;
+        public bool inputActive;
+
+        public player()
+        {
+
+        }
+
+        public void input()
+        {
+            KeyboardState keyboard = Keyboard.GetState();
+            if (inputActive)
+            {
+                if (keyboard.IsKeyDown(Keys.Right) && x < 800-37)
+                {
+                    x += 5;
+                }
+                if (keyboard.IsKeyDown(Keys.Left) && x > 0)
+                {
+                    x -= 5;
+                }
+                if (keyboard.IsKeyDown(Keys.Up) && y > 0)
+                {
+                    y -= 5;
+                }
+                if (keyboard.IsKeyDown(Keys.Down) && y < 800-37)
+                {
+                    y += 5;
+                }
+            }
+        }
+    }
+}
